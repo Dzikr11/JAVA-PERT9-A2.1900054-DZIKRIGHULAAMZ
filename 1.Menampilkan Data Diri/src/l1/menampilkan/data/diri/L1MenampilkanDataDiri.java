0eb9b8e305f83265be5b218acd8982609c6ef2f3
Dzikr11 <english9.ghulaam@gmail.com>
2019-11-22 15:19:08 +0700

    JAVA-PERT9-A2.1900054-DZIKRIGHULAAMZ

---
+++ 1.Menampilkan Data Diri/src/l1/menampilkan/data/diri/L1MenampilkanDataDiri.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package l1.menampilkan.data.diri;
+
+/**
+ * NAMA  : Dzikri Ghulaam Zakiyya
+ * NIM   : A2.1900054
+ * Kelas : TI-1C
+ * 
+ */
+public class L1MenampilkanDataDiri {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Biodata" + "\n" + "-----------------" );
+        System.out.println("NAMA\t\t           : Dzikri Ghulaam Z");
+        System.out.println("ALAMAT\t\t         : Baginda ");
+        System.out.println("TEMPAT LAHIR\t\t : Sumedang ");
+        System.out.println("TANGGAL LAHIR\t\t : 11 November 2001");
+        System.out.println("--------------------------");
+        System.out.println("HOBI\t\t: Membaca");
+        System.out.println("MAKES\t\t: Nasgor");
+        System.out.println("MIKES\t\t: kopi");
+    }
+    
+}
